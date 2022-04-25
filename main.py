32f25e21efefece507c031716537881e57a94cd8
sarthak khokhar <90543827+SarthakK337@users.noreply.github.com>
2022-04-26 00:38:52 +0530

    first commit

--- main.py
+++ main.py
@@ -114,6 +114,15 @@ def dataanalysis():
         return Response("Error Occurred! %s" % KeyError)
     except Exception as e:
         return Response("Error Occurred! %s" % e)
+    
+port = int(os.getenv("PORT", 5000))
+if __name__ == "__main__":
+    host = '0.0.0.0'
+    # port = 5000
+    httpd = simple_server.make_server(host, port, app)
+    # print("Serving on %s %d" % (host, port))
+    httpd.serve_forever()
+
 
 
 
